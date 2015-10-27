2dc67259ce84e8e2b9f8b266004ec408079db0f5
thelaui <felix.lauer@uni-weimar.de>
2015-10-27 15:24:45 +0100

    added hacky clamping of splat size

--- plugins/guacamole-plod/resources/shaders/gbuffer/plod/common_PLOD/PLOD_assign_tangents.glsl
+++ plugins/guacamole-plod/resources/shaders/gbuffer/plod/common_PLOD/PLOD_assign_tangents.glsl
@@ -4,6 +4,13 @@
   //VertexOut.pass_ms_v = normalize(tmp_ms_v) * 2.0 * in_radius ;
 
   //adrian
-VertexOut.pass_ms_u = normalize(ms_u) * radius_scaling * in_radius;
-VertexOut.pass_ms_v = normalize(cross(ms_n, ms_u)) * radius_scaling  * in_radius;
-  
+
+float clamped_radius = in_radius;
+
+if (in_radius > 0.1) {
+  clamped_radius = 0.0;
+}
+
+VertexOut.pass_ms_u = normalize(ms_u) * radius_scaling * clamped_radius;
+VertexOut.pass_ms_v = normalize(cross(ms_n, ms_u)) * radius_scaling  * clamped_radius;
+
