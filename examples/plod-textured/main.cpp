4e381e6f13bf1857f5efdd3a2610d3f11b5c66ba
thelaui <felix.lauer@uni-weimar.de>
2015-10-27 15:43:56 +0100

    added histogram streching/equalization

--- examples/plod-textured/main.cpp
+++ examples/plod-textured/main.cpp
@@ -813,13 +813,12 @@ int main(int argc, char** argv) {
       cv::Mat screen_shot(resolution.y, resolution.x, CV_32FC3, data.data());
       cv::flip(screen_shot, screen_shot, 0); //flip around x axis
       cv::cvtColor(screen_shot, screen_shot, CV_BGR2GRAY); //convert from bgr to rgb color space
-      screen_shot.convertTo(screen_shot, CV_32FC1);
-      cv::normalize(screen_shot, screen_shot, 0.0, 1.0, cv::NORM_MINMAX);
+      screen_shot.convertTo(screen_shot, CV_8UC1, 255.0);
+      cv::equalizeHist(screen_shot, screen_shot);
       // cv::imshow("screen shot", screen_shot);
 
       cv::Mat mask;
-      cv::threshold(screen_shot, mask, 0, 1, cv::THRESH_BINARY);
-      mask.convertTo(mask, CV_8U, 255.0);
+      cv::threshold(screen_shot, mask, 0, 255, cv::THRESH_BINARY);
       // cv::imshow("mask", mask);
 
       cv::Mat photo(cv::imread(frusta[current_frustum].get_image_file_name(), CV_LOAD_IMAGE_GRAYSCALE));
@@ -828,12 +827,13 @@ int main(int argc, char** argv) {
       cv::Mat masked_photo;
 
       photo.copyTo(masked_photo, mask);
-      masked_photo.convertTo(masked_photo, CV_32FC1, 1.0/255.0);
-      // cv::normalize(photo, photo, 0.0, 1.0, cv::NORM_MINMAX);
-      cv::imshow("photography", masked_photo);
+      masked_photo.convertTo(masked_photo, CV_8UC1);
+      cv::equalizeHist(masked_photo, masked_photo);
+      // cv::imshow("photography", masked_photo);
 
       cv::Mat diff;
       cv::absdiff(screen_shot, masked_photo, diff);
+      diff.convertTo(diff, CV_32FC1, 1.0/255.0);
       cv::imshow("difference", diff);
       std::cout << cv::sum(diff) << std::endl;
 
